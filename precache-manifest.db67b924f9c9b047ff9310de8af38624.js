22a07627ab71b3cb1382a1f2b640d6f304e0957c
peter rader <raderp841@gmail.com>
2020-06-05 01:16:43 -0400

    Updates

---
+++ precache-manifest.db67b924f9c9b047ff9310de8af38624.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5268f1d588381b75f2c266837c38ee85",
+    "url": "/ddmReact/index.html"
+  },
+  {
+    "revision": "ad50dd22312056a4bbc3",
+    "url": "/ddmReact/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "98b2d2d55733585ea81d",
+    "url": "/ddmReact/static/css/main.505de3b9.chunk.css"
+  },
+  {
+    "revision": "ad50dd22312056a4bbc3",
+    "url": "/ddmReact/static/js/2.357069cd.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/ddmReact/static/js/2.357069cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98b2d2d55733585ea81d",
+    "url": "/ddmReact/static/js/main.cc21ea9b.chunk.js"
+  },
+  {
+    "revision": "46bf911285de706bdf64",
+    "url": "/ddmReact/static/js/runtime-main.fcbeef97.js"
+  }
+]);
